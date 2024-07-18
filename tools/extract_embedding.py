efd07a6654a5418c56b1a18d154d34354dd558cf
Scottish_Fold007 <gaochangkuan01@foxmail.com>
2024-07-18 12:16:09 +0800

    Update extract_embedding.py
    应对embedding提取为nan的情况

--- tools/extract_embedding.py
+++ tools/extract_embedding.py
@@ -1,17 +1,5 @@
 #!/usr/bin/env python3
-# Copyright (c) 2024 Alibaba Inc (authors: Xiang Lyu)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+
 import argparse
 import torch
 import torchaudio
@@ -33,13 +21,17 @@ def main(args):
 
     option = onnxruntime.SessionOptions()
     option.graph_optimization_level = onnxruntime.GraphOptimizationLevel.ORT_ENABLE_ALL
-    option.intra_op_num_threads = 1
+    option.intra_op_num_threads = 16
     providers = ["CPUExecutionProvider"]
+    #providers = ["CUDAExecutionProvider"]  # 设置提供者为CUDA
     ort_session = onnxruntime.InferenceSession(args.onnx_path, sess_options=option, providers=providers)
 
     utt2embedding, spk2embedding = {}, {}
     for utt in tqdm(utt2wav.keys()):
-        audio, sample_rate = torchaudio.load(utt2wav[utt])
+        try:
+            audio, sample_rate = torchaudio.load(utt2wav[utt])
+        except:
+            print("Bad wav path: ", utt2wav[utt])
         if sample_rate != 16000:
             audio = torchaudio.transforms.Resample(orig_freq=sample_rate, new_freq=16000)(audio)
         feat = kaldi.fbank(audio,
@@ -48,13 +40,28 @@ def main(args):
                            sample_frequency=16000)
         feat = feat - feat.mean(dim=0, keepdim=True)
         embedding = ort_session.run(None, {ort_session.get_inputs()[0].name: feat.unsqueeze(dim=0).cpu().numpy()})[0].flatten().tolist()
-        utt2embedding[utt] = embedding
+        if not torch.isnan(torch.tensor(embedding)).any():
+            utt2embedding[utt] = embedding
+        else:
+            utt2embedding[utt] = []
         spk = utt2spk[utt]
         if spk not in spk2embedding:
             spk2embedding[spk] = []
-        spk2embedding[spk].append(embedding)
-    for k, v in spk2embedding.items():
-        spk2embedding[k] = torch.tensor(v).mean(dim=0).tolist()
+        if len(embedding) != 0:
+            spk2embedding[spk].append(embedding)
+        
+    #for k, v in spk2embedding.items():
+    #    tensor_v = torch.tensor(v)
+    #    spk2embedding[k] = torch.tensor(tensor_v).mean(dim=0).tolist()
+    
+    for k, embeddings_list in spk2embedding.items():
+        filtered_embeddings = [emb for emb in embeddings_list if not torch.isnan(torch.tensor(emb)).any()]
+        if filtered_embeddings:
+            spk2embedding[k] = torch.tensor(filtered_embeddings).mean(dim=0).tolist()
+        else:
+            #raise ValueError(f"The embeddings for speaker '{k}' are all NaN or the list is empty.")
+            spk2embedding[k] = []
+
 
     torch.save(utt2embedding, '{}/utt2embedding.pt'.format(args.dir))
     torch.save(spk2embedding, '{}/spk2embedding.pt'.format(args.dir))
@@ -67,3 +74,5 @@ if __name__ == "__main__":
                         type=str)
     args = parser.parse_args()
     main(args)
+    
+    
