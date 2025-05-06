51bc582fed6dac49336d640e8e2e40c735878cd1
Scottish_Fold007 <gaochangkuan01@foxmail.com>
2025-05-06 15:09:48 +0800

    Update encoder_layer.py

--- cosyvoice/transformer/encoder_layer.py
+++ cosyvoice/transformer/encoder_layer.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2021 Mobvoi Inc (Binbin Zhang, Di Wu)  
 #               2022 Xingchen Song (sxc19@mails.tsinghua.edu.cn)  
-#               2024 修改以支持NVIDIA TransformerEngine FP8  
+#               2025 修改以支持NVIDIA TransformerEngine FP8  
 #  
 # Licensed under the Apache License, Version 2.0 (the "License");  
 # you may not use this file except in compliance with the License.  
