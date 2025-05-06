2e50acb727c172458e4c0e9e3c97dc8dff6a774d
Scottish_Fold007 <gaochangkuan01@foxmail.com>
2025-05-06 15:14:02 +0800

    Update embedding.py

--- cosyvoice/transformer/embedding.py
+++ cosyvoice/transformer/embedding.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2020 Mobvoi Inc. (authors: Binbin Zhang, Di Wu)  
 #               2024 Alibaba Inc (Xiang Lyu)  
-#               2024 修改以支持NVIDIA TransformerEngine FP8  
+#               2025 修改以支持NVIDIA TransformerEngine FP8  
 #  
 # Licensed under the Apache License, Version 2.0 (the "License");  
 # you may not use this file except in compliance with the License.  
